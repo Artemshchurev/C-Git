49be61f075b8cb4e258b4a501484a2a990f0c2ff
artemshchurev <artemshchurev@gmail.com>
2018-01-23 15:30:35 +1000

    Добавил фрактал и вектор

---
+++ MyClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+        public Vector(double X, double Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+    }
+    public static class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2));
+        }
+        public static Vector Add(Vector firstVector, Vector secondVector)
+        {
+            return new Vector(firstVector.X + secondVector.X, firstVector.Y + secondVector.Y);
+        }
+    }
+}
