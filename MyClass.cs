2e61ac5324fdbd518e2ebb2c5c5c73ec239a896d
artemshchurev <artemshchurev@gmail.com>
2018-03-05 23:51:47 +1000

    Последняя версия

--- MyClass.cs
+++ MyClass.cs
@@ -1,25 +1,71 @@
 ﻿using System;
+using GeometryPainting;
 namespace GeometryTasks
 {
     public class Vector
     {
         public double X;
         public double Y;
-        public Vector(double X, double Y)
+        public Vector()
         {
-            this.X = X;
-            this.Y = Y;
+        }
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+        public Vector Add(Vector vector)
+        {
+            return Geometry.Add(this, vector);
+        }
+        public Boolean Belongs(Segment segment)
+        {
+            return Geometry.IsVectorInSegment(this, segment);
         }
     }
     public static class Geometry
     {
         public static double GetLength(Vector vector)
         {
-            return Math.Sqrt(Math.Pow(vector.X, 2) + Math.Pow(vector.Y, 2));
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+        public static double GetLength(Segment segment)
+        {
+            var vector = new Vector();
+            vector.X = segment.End.X - segment.Begin.X;
+            vector.Y = segment.End.Y - segment.Begin.Y;
+            return GetLength(vector);
+        }
+        public static Boolean IsVectorInSegment(Vector vector, Segment segment)
+        {
+            //(x-x1)(y2-y1)-(y-y1)(x2-x1) = 0
+            return Equals((vector.X - segment.Begin.X) * (segment.End.Y - segment.Begin.Y),
+                          (vector.Y - segment.Begin.Y) * (segment.End.X - segment.Begin.X)) &&
+                vector.X >= segment.Begin.X && vector.X <= segment.End.X &&
+                      vector.Y >= segment.Begin.Y && vector.Y <= segment.End.Y;
         }
         public static Vector Add(Vector firstVector, Vector secondVector)
         {
-            return new Vector(firstVector.X + secondVector.X, firstVector.Y + secondVector.Y);
+            var vector = new Vector();
+            vector.X = firstVector.X + secondVector.X;
+            vector.Y = firstVector.Y + secondVector.Y;
+            return vector;
+        }
+    }
+    public class Segment
+    {
+        public Vector Begin;
+        public Vector End;
+        public Segment()
+        {
+
+        }
+        public double GetLength()
+        {
+            return Geometry.GetLength(this);
+        }
+        public Boolean Contains(Vector vector)
+        {
+            return Geometry.IsVectorInSegment(vector, this);
         }
     }
 }
