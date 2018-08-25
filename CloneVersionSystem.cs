cebd3d10ed3dbd67f7db3be9fbf1d7ae031ec821
artemshchurev <artemshchurev@gmail.com>
2018-08-25 23:19:38 +1000

    Реализовал методы rollback, relearn, learn.

--- CloneVersionSystem.cs
+++ CloneVersionSystem.cs
@@ -3,46 +3,89 @@ using System.Collections.Generic;
 
 namespace Clones
 {
-	public class CloneVersionSystem : ICloneVersionSystem
+    public class CloneVersionSystem : ICloneVersionSystem
     {
-        public Dictionary<string, Clone> clones = new Dictionary<string, Clone>()
+        public Dictionary<string, CaminClone> clones = new Dictionary<string, CaminClone>()
         {
-            {"1", new Clone()}
+            {"1", new CaminClone()}
         };
 
-		public string Execute(string query)
-		{
+        public string Execute(string query)
+        {
             var command = query.Split(' ');
-            switch(command[0]){
+            switch (command[0])
+            {
                 case "learn":
                     return clones[command[1]].Learn(command[2]);
+                case "rollback":
+                    return clones[command[1]].Rollback();
+                case "relearn":
+                    return clones[command[1]].Relearn();
+                case "clone":
+                    return MakeClone(command[1]);
                 case "check":
                     return clones[command[1]].GetLastLearnedProgram();
 
             }
- 			return null;
-		}
-	}
-    public class Clone
+            return null;
+        }
+
+        private string MakeClone(string stringCloneNumber)
+        {
+            int intCloneNumber = int.Parse(stringCloneNumber);
+            intCloneNumber++;
+            string stringNewCloneNumber = intCloneNumber.ToString();
+            CaminClone newClone = (CaminClone)clones[stringCloneNumber].Clone();
+            clones.Add(stringNewCloneNumber, newClone);
+            return null;
+        }
+
+    }
+    public class CaminClone : ICloneable
     {
+        public CaminClone()
+        {
+            this.learnedPrograms = new Queue();
+            this.rollBackedPrograms = new Queue();
+        }
         public Queue learnedPrograms;
         public Queue rollBackedPrograms;
+
         public string Learn(String program)
         {
             this.learnedPrograms.Enqueue(program);
             return null;
         }
-        public string GetLastLearnedProgram(){
+
+        public string Rollback()
+        {
+            this.rollBackedPrograms.Enqueue(this.learnedPrograms.Dequeue());
+            return null;
+        }
+
+        public string Relearn()
+        {
+            this.learnedPrograms.Enqueue(this.rollBackedPrograms.Dequeue());
+            return null;
+        }
+
+        public string GetLastLearnedProgram()
+        {
             var lastProgram = learnedPrograms.GetTail();
             if (lastProgram == null) return "basic";
             return lastProgram;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
         }
     }
 
     public class CloneProgram
     {
         public string Value { get; set; }
-        public CloneProgram Next { get; set; }
+        public CloneProgram Prev { get; set; }
     }
 
     public class Queue
@@ -53,11 +96,11 @@ namespace Clones
         public void Enqueue(string value)
         {
             if (head == null)
-                tail = head = new CloneProgram { Value = value, Next = null };
+                tail = head = new CloneProgram { Value = value, Prev = null };
             else
             {
-                var item = new CloneProgram { Value = value, Next = null };
-                tail.Next = item;
+                var item = new CloneProgram { Value = value, Prev = null };
+                item.Prev = tail;
                 tail = item;
             }
         }
@@ -65,15 +108,18 @@ namespace Clones
         public string Dequeue()
         {
             if (head == null) throw new InvalidOperationException();
-            var result = head.Value;
-            head = head.Next;
-            if (head == null)
-                tail = null;
+            var result = tail.Value;
+            tail = tail.Prev;
+            if (tail == null)
+                head = null;
             return result;
         }
 
-        public string GetTail(){
+        public string GetTail()
+        {
+            if (tail == null) return null;
             return tail.Value;
         }
     }
+
 }
